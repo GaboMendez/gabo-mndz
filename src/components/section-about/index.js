ce393c5907c17204cc5ee14b7a8f9e4496536b77
GabrielMendez <gabriel.mendez0@hotmail.com>
2021-11-16 21:48:12 -0400

    Update index.js

--- src/components/section-about/index.js
+++ src/components/section-about/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { aboutObj } from '../section-info/data';
+import {
+  Column01,
+  Heading,
+  SubTitle,
+  TextWrapper,
+  TopLine,
+} from '../section-info/styles';
 import { AboutWrapper, ColumnContent } from './styles';
 
 const AboutSection = () => {
+  const { topLine, lightText, headLine, darkText, description } = {
+    ...aboutObj,
+  };
+
   return (
     <>
       <AboutWrapper id='about'>
         <ColumnContent>
-          <h2>Hey!!</h2>
+          <Column01>
+            <TextWrapper>
+              <TopLine> {topLine} </TopLine>
+              <Heading lightText={lightText}> {headLine} </Heading>
+              <SubTitle darkText={darkText}> {description} </SubTitle>
+            </TextWrapper>
+          </Column01>
         </ColumnContent>
 
         <ColumnContent>
