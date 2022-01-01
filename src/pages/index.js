ac657c5dca76be765b55d4b8347c0a8cce0535b7
GabrielMendez <gabriel.mendez0@hotmail.com>
2021-12-31 20:09:03 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import AboutSection from '../components/section-about';
-import HeroSection from '../components/section-hero';
-import InfoSection from '../components/section-info';
-import { aboutObj, discoverObj } from '../components/section-info/data';
-import SkillsSection from '../components/section-skills';
 import ContactSection from '../components/section-contact';
+import HeroSection from '../components/section-hero';
+import SkillsSection from '../components/section-skills';
 
 export default function Home() {
   return (
