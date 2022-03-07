a38020af8b24e2f3ae0e235001ed691638800ce2
GabrielMendez <gabriel.mendez0@hotmail.com>
2022-03-07 00:49:28 -0400

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,35 +1,45 @@
+import { ServerStyleSheets } from '@material-ui/styles';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheets } from '@material-ui/core/styles';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheets = new ServerStyleSheets();
+    const styledComponentsSheet = new ServerStyleSheet();
+    const materialSheets = new ServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
 
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-      });
-
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return {
-      ...initialProps,
-      // Styles fragment is rendered after the app and page rendering finish.
-      styles: [
-        ...React.Children.toArray(initialProps.styles),
-        sheets.getStyleElement(),
-      ],
-    };
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            styledComponentsSheet.collectStyles(
+              materialSheets.collect(<App {...props} />)
+            ),
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <React.Fragment>
+            {initialProps.styles}
+            {materialSheets.getStyleElement()}
+            {styledComponentsSheet.getStyleElement()}
+          </React.Fragment>
+        ),
+      };
+    } finally {
+      styledComponentsSheet.seal();
+    }
   }
 
   render() {
     return (
-      <Html lang='en'>
-        <Head />
+      <Html lang='en' dir='ltr'>
+        <Head>
+          <meta charSet='utf-8' />
+        </Head>
         <body>
-          <script>0</script>
           <Main />
           <NextScript />
         </body>
